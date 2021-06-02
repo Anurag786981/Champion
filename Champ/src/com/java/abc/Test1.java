af8b9c20b666064733d61c4b1db15e50b9265e39
lenovo <lenovo@ANURAG>
2021-06-02 16:58:33 +0530

    First Commit

---
+++ Champ/src/com/java/abc/Test1.java
@@ -0,0 +1,17 @@
+package com.java.abc;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungryString = "yes";
+
+		if (hungryString == "yes")
+			System.out.println("eat Somthing");
+
+		else
+			System.out.println("do work");
+	}
+	
+
+}
